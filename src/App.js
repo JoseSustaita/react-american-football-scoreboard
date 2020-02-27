88ba5825f8b3c52894def0e5dc7984d428a9e8c2
Jose Sustaita <josesustaita70@gmail.com>
2020-02-26 20:09:39 -0500

    Stretch complete
    Added useState hook for quater value, then changed the hard coded values to dynamic and added functions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [lionsScore, setLionsScore] = useState(0);
   const [tigersScore, setTigersScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+
+  if (quarter > 4) {
+    setQuarter(1);
+  }
   return (
     <div className="container">
       <section className="scoreboard">
@@ -24,6 +29,10 @@ function App() {
   <div className="away__score">{tigersScore}</div>
           </div>
         </div>
+        <div className="quarter">
+            <h3 className="quarter__title">Quarter</h3>
+            <div className="quarter__value">{quarter}</div>
+          </div>
         <BottomRow />
       </section>
       <section className="buttons">
@@ -35,6 +44,9 @@ function App() {
         <div className="awayButtons">
           <button className="awayButtons__touchdown"onClick={() => setTigersScore(tigersScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal"onClick={() => setTigersScore(tigersScore + 3)}>Away Field Goal</button>
+          <div className="gameButtons">
+          <button className="gameButtons__changeQuarter" onClick={() => setQuarter(quarter + 1)}>Change Quarter</button>
+        </div>
         </div>
       </section>
     </div>
